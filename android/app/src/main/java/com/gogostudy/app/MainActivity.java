36b0393e3b0e46d71c1e712705f2de71b9b2f4d6
chanhui2837 <chanhui.lee2837@gmail.com>
2026-09-21 18:30:29 +0900

    앱 전체화면(몰입모드) + 프로필 사진 label 방식 수정

--- android/app/src/main/java/com/gogostudy/app/MainActivity.java
+++ android/app/src/main/java/com/gogostudy/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
@@ -24,6 +25,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemUI();
         setContentView(R.layout.activity_main);
         web = findViewById(R.id.web);
         WebSettings st = web.getSettings();
@@ -80,5 +82,23 @@ public class MainActivity extends Activity {
     public void onBackPressed() {
         if (web.canGoBack()) web.goBack();
         else super.onBackPressed();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemUI();
+    }
+
+    // 하단 3버튼 + 상단 상태바 숨기기 (몰입형 전체화면)
+    private void hideSystemUI() {
+        View decor = getWindow().getDecorView();
+        int flags = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        decor.setSystemUiVisibility(flags);
     }
 }
